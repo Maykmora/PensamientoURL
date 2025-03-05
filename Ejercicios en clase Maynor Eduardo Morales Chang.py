d76fe2e06f399be52fe8878805282fafc556d3f6
Maykmora <maykmora12@gmail.com>
2025-03-05 11:35:23 -0600

    Add files via upload

---
+++ Ejercicios en clase Maynor Eduardo Morales Chang.py
@@ -0,0 +1,63 @@
+#Primer ejercicio 
+#print("Inicio del programa")
+
+#texto=str(input("Ingrese su oracion: "))
+
+#palabra=texto.split()
+
+#print("Su primer palabra es:", palabra[0])
+#print("Su ultima palabra es:", palabra[-1]) 
+
+#Segundo ejercicio 
+
+#texto=str(input("Ingrese su oracion: "))
+#v2=" ".join(texto.split())
+#print(v2)
+
+
+#Tercer ejercicio 
+#correo=str(input("Ingrese su correo:"))
+#dominio=correo.split("@")[1]
+
+#print("El dominio de su correo es:", dominio)
+
+
+#Cuarto ejercicio 
+
+#documento=str(input("Ingrese su documento: "))
+
+#if documento.endswith((".pdf", ".docx", ".jpg", ".png")):
+    #print("El documento tiene una extensión correcta")
+#else:
+    #print("El documento tiene una extensión incorrecta")
+    
+    
+#Quinto Ejercicio 
+
+#texto=str(input("Igrese la oración que necesite invertir:"))
+#v2=" ".join(texto.split()[::-1])
+
+#print("El texto invertido es:", v2)
+
+    
+    
+#Sexto Ejercicio 
+
+
+# Definir cadenas predefinidas
+#p1=["Podrá nublarse el sol eternamente; podrá secarse en un instante el mar; etc.","El amor es como un río que fluye sin cesar, y en su cauce se encuentra la paz. etc.","Si te amo, es porque te quiero; si te quiero, es porque te amo. etc.","Eres la flor que mi alma cuida, y el amor que me mantiene vivo. etc.","Nuestro amor es eterno, como el sol que nunca deja de brillar. "]
+#c1=["Eres como la noche, cálida y constelada; tu silencio es de estrella, tan lejano y sencillo. etc.","Hoy el sol brilla más, el mundo canta en libertad, y mi corazón da saltos de felicidad. etc.","La vida es una canción, y mi alma canta con alegría, todo es posible cuando tienes esperanza. etc.","Quiero bailar bajo la lluvia, reír sin parar, y sentir que el mundo es mío, lleno de paz. etc.","Las estrellas nos miran sonriendo, la luna brilla con fuerza, todo es alegría en mi ser."]
+
+
+#ingreso=str(input("Ingrese que necesita")) 
+#texto=ingreso.lower()
+    
+#if 'poema' in ingreso and 'amor' in ingreso:
+    #print(p1)
+#elif 'cancion' in texto and 'alegria' in texto:
+   # print(c1)
+#else:
+    #print("No se detectaron palabras clave adecuadas. Intenta nuevamente.") 
+        
+    
+    
