a9711727e7527ef2ddc516ea1f53eaf20f123a79
Maykmora <maykmora12@gmail.com>
2025-03-12 11:22:11 -0600

    Add files via upload

---
+++ Ejercicio laboratorio- Maynor Eduardo Morales Chang.py
@@ -0,0 +1,52 @@
+#Primer ejercicio 
+
+#m=int(input("Ingrese la cantidad de metros cubicos consumidos:"))
+#p=int(input("Ingrese la cantidad de habitantes en el apartamento:"))
+
+#if m<15 and m>=0:
+    #print("Su pago total en quetzales es:", m*5)
+
+#elif m>=15 and m<=30 and p>=0:
+    #if p>3: 
+        #print("Su pago total en quetzales es:", m*4)
+    #elif p==3:
+        #print("Su pago total en quetzales es:", m*4.5)
+    #else:
+        #print("Su pago total en quetzales es:", m*5)
+
+#elif m>30 and p>=0:
+    #if p>5:
+        #print("Su pago total en quetzales es:", m*3.5)
+    #elif p%2==0:
+        #print("Su pago total en quetzales es:", m*4)
+    #else:
+        #print("Su pago total en quetzales es:", m*4.2)
+
+#else:
+    #print("Erro al ingresar un dato, corrobore e intete de nuevo.")
+    
+    
+    
+#Segundo ejercicio
+
+#p=int(input("Ingrese los numeros de la placa del automovil:"))
+#a=int(input("Ingrese el año del automovil:"))
+
+#d=p%10
+#if a>2001:
+    #if d==0 or d==2 or d==4 or d==6 or d==8:
+        #print("El automovil no puede circular los lunes.")
+    #elif d==1 or d==3 or d==5 or d==7 or d==9:
+        #print("El automovil no puede circular los viernes")
+
+    #elif (a%2)==0:
+        #print("EL automovil puede circular los sabados hasta medio día ")
+    
+#elif a-2000<0:
+   #print("Advertencia: El vehiculo necesita mantenimiento obligatorio")
+    
+#else:
+    #print("Error al ingresar un dato, intentelo de nuevo.")
+
+
+    
