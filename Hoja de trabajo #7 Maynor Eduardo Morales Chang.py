605ab36ad2a62b3a6376239a4d065e3b10036237
Maykmora <maykmora12@gmail.com>
2025-03-26 11:22:33 -0600

    Add files via upload

---
+++ Hoja de trabajo #7 Maynor Eduardo Morales Chang.py
@@ -0,0 +1,31 @@
+#Primer Ejercicio 
+#i=0
+#for i in range(1,11,2):
+    #print("los numeros impares son:", i)
+
+
+#Segundo Ejercicio 
+#x = 1
+#while x < 11:
+    #if x%2 !=0:
+        #print("Los numeros impares son", x)
+    #x+=1
+
+#Tercer Ejercicio
+#while True:
+    #palabra = str(input("Ingresa una palabra : "))
+    #if palabra == "chupacabra":
+        #print("¡Has dejado el bucle con éxito!")
+        #break
+
+#Cuarto Ejercicio 
+# user_word = str(input("Ingresa una palabra: "))
+# user_word = user_word.upper()
+
+# for letter in user_word:
+#     if letter in ['A', 'E', 'I', 'O', 'U']:
+#         continue
+#     print(letter)
+
+
+#Quinto ejercicio
